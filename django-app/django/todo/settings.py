9d5cddae8467a6123e0b4984435d99915cc184d4
Mykhailo Kuzich <timmkedth@gmail.com>
2025-11-16 22:43:09 +0100

    added sqlite

--- django-app/django/todo/settings.py
+++ django-app/django/todo/settings.py
@@ -77,12 +77,14 @@ WSGI_APPLICATION = "todo.wsgi.application"
 DATABASES = {
 
 	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-		'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-		'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
-		'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-		'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+		# 'ENGINE': 'django.db.backends.postgresql',
+		# 'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+		# 'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+		# 'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+		# 'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+		# 'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
 	}
 }
 
